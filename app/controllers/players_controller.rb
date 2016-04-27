fe4833263511e9277c3eb1d54c3dacd637646ad1
ROR Future <rorfuture@gmail.com>
2016-04-27 20:45:46 +0530

    commit

---
+++ app/controllers/players_controller.rb
@@ -0,0 +1,20 @@
+class PlayersController < ApplicationController
+
+	def index
+		gon.players=generatePlayers()
+	end
+
+
+private
+
+  #generats some faker player data
+  def generatePlayers()
+  	res=[]
+  	(1..20).each do |i|
+  		obj={name:Faker::Name.name, email:Faker::Internet.email, logo:Faker::Company.logo, description:Faker::Lorem.sentence, image: Faker::Avatar.image()
+}
+  		res << obj
+  	end
+  	return res
+  end
+end
